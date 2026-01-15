887dca99b7472d383b679c33e5701066d98a194c
anwaraneeqa60-lang <anwaraneeqa60@gmail.com>
2026-01-14 20:21:16 -0500

    first commit

---
+++ and and or operators.py
@@ -0,0 +1,22 @@
+a = 10 
+b = 12
+c = 15
+if a and b and c:
+    print("all the numbers have boolean as true")
+else:
+    print("one number that has boolean value as false")
+
+
+a = 18
+b = -10
+c = 0
+if a > 0 and b > 0:
+    print("either number is greater than 0")
+else:
+    print("no number is greater than 0")
+
+
+if a > 0 or b > 0:
+    print("either of the number is greater than 0")
+else:
+    print("no number is greater than 0")
