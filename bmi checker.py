bb7fb410e813413b649addc8cf741738e9dd823a
anwaraneeqa60-lang <anwaraneeqa60@gmail.com>
2026-01-16 18:57:52 -0500

    checking alphabets.py

---
+++ bmi checker.py
@@ -0,0 +1,4 @@
+height = float(input("enter your height here in cm :"))
+weight = float(input("enter your weight in kg"))
+bmi = weight  /   (height/100)**2
+print("your bmi is", bmi)
---
+++ checking alphebets.py
@@ -0,0 +1,7 @@
+ayaan = 40
+hooram = 80
+sultan = 12
+if ayaan and hooram and sultan:
+    print("the character entered by user is a alphabet ")
+else:
+    print("the character entered by user is not and alphabet")
